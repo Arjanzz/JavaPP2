2c06b81f8b6ef4db10f7fbb4a511610e3a61b757
Hotsko Vladimir <malletgamer@mail.ru>
2022-05-03 12:45:02 +0300

    try2

--- src/main/java/jm/task/core/jdbc/dao/UserDao.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDao.java
@@ -1,6 +1,8 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public interface UserDao {
@@ -8,11 +10,12 @@ public interface UserDao {
 
     void dropUsersTable();
 
-    void saveUser(String name, String lastName, byte age);
+    void saveUser(String name, String lastName, byte age) throws SQLException;
 
-    void removeUserById(long id);
+    void removeUserById(long id) throws SQLException;
 
-    List<User> getAllUsers();
+    List<User> getAllUsers() throws SQLException;
 
-    void cleanUsersTable();
+    void cleanUsersTable() throws SQLException;
+
 }
